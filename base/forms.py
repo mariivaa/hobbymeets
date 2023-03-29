16918e199f78d1e2c5fa53dde0e9c6be5d59b8c0
mariivaa <ivma96@gmail.com>
2023-03-29 10:29:36 +0200

    add functionality to update user profile

--- base/forms.py
+++ base/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm #"Kind of like a class-based representation of a form" - does some of the work for ya
 from .models import Room
+from django.contrib.auth.models import User
 
 
 class RoomForm(ModelForm):
@@ -7,3 +8,9 @@ class RoomForm(ModelForm):
         model = Room
         fields = '__all__' # create all fields that are specified in the Room model (except for fields that aren't possible to update/change manually, eg. updated/changed)
         exclude = ['host', 'participants'] #one way to remove some fields from the form. remember to add these values in views.py so that something will be sent to the db
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout 
 from django.contrib.auth.forms import UserCreationForm 
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 '''
 REQUESTS explained: When a page is requested, Django creates an HttpRequest object that contains metadata about the request. 
 Then Django loads the appropriate view, passing the HttpRequest as the first argument to the view function. 
@@ -182,3 +182,18 @@ def deleteMessage(request, pk):
         message.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj' : message})
+
+
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    return render(request, 'base/update_user.html', {'form': form})
